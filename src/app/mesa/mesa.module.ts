eccb3adbe9fb62ad29fb5bc0e0393262f0dd9370
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-01-06 18:56:35 -0300

    se crea modulos empleado,menu,mesa y pedido

---
+++ src/app/mesa/mesa.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MesaRoutingModule } from './mesa-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MesaRoutingModule
+  ]
+})
+export class MesaModule { }
