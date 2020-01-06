689b82ce73eaf16804a837ee0c549e08e189f7a7
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-01-06 16:03:13 -0300

    se genera el footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
+
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 import { MaterializeModule } from '../materialize/materialize.module';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule,
-    MaterializeModule
+    MaterializeModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
