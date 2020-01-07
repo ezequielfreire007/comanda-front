f7756cce99a07255a9e24ccac4765929740ed48d
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-01-07 14:42:22 -0300

    se agrega img

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MenuService } from './services/menu/menu.service';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    MenuService
+  ]
+})
+export class CoreModule { }
