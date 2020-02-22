7b4bf601bace3ee1b6e77ee48b7646ee98ddb25f
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-02-22 11:52:31 -0300

    se quita form de app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -27,7 +26,6 @@ export function getAccessToken() {
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    FormsModule,
     HttpClientModule,
     [JwtModule.forRoot({
       config: {
