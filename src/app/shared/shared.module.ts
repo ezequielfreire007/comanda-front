cd14be1c4c24413186fe2803cb6cc4387fbfd9e5
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-02-22 11:48:29 -0300

    se agrega form a shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Router } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -17,12 +17,14 @@ import { ValidarRolesDirective } from './directives/validar-roles/validar-roles.
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    FormsModule
   ],
   imports: [
     CommonModule,
     MaterializeModule,
-    RouterModule
+    RouterModule,
+    FormsModule,
   ]
 })
 export class SharedModule { }
