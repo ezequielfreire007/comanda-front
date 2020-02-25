2a89f03b4aa7949bbd17057f88c278d9482afa42
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-02-25 00:03:30 -0300

    se modifica header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../core/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.auth.logoutEmpleado();
+    localStorage.removeItem('empleado');
+    localStorage.removeItem('pedido');
+    this.router.navigate(['/login']);
   }
 
 }
