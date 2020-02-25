bce3a179a0db90656b52d74a7cdf66cae33251b3
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-02-25 00:09:18 -0300

    se pasa local storage de empleado

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthService } from '../../../core/services/auth/auth.service';
 import { Login } from 'src/app/core/models/login';
-
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
   selector: 'app-login',
@@ -18,6 +18,7 @@ export class LoginComponent implements OnInit {
   login: Login;
   error: boolean;
   errorMessage: string;
+  helper = new JwtHelperService();
 
   constructor(
     private formBuilder: FormBuilder,
@@ -30,54 +31,29 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     if (this.auth.loggedIn()) {
-      console.log('usuario ya logeador direccionar a empleados');
       this.router.navigate(['/pedido']);
     }
   }
 
   loginUser(event: Event) {
-    //debugger;
     this.error = false;
     if (this.form.valid) {
-    const dataLogin: Login = { nombre_empleado : `${this.form.get('nombre').value}`, clave_empleado: `${this.form.get('password').value}`};
-    console.log(dataLogin);
-    this.auth.loginEmpleado(dataLogin).subscribe(res => {
-      console.log(res);
-      if (res['estado'] === 'ok') {
-        console.log('el estado es ok');
-        localStorage.setItem('token', res['token']);
-
-        if (!this.auth.redirectUrl) {
-          this.auth.redirectUrl = 'predido';
+      // tslint:disable-next-line: max-line-length
+      const dataLogin: Login = { nombre_empleado : `${this.form.get('nombre').value}`, clave_empleado: `${this.form.get('password').value}`};
+      this.auth.loginEmpleado(dataLogin).subscribe(res => {
+        if (res['estado'] === 'ok') {
+          // seteo el token en local storage
+          localStorage.setItem('token', res['token']);
+          // decodifico el token y tomo la data para que este disponible
+          const decodeToken =  this.helper.decodeToken(localStorage.getItem('token'));
+          localStorage.setItem('empleado', JSON.stringify(decodeToken.empleado));
+          // redireciono a pedido todos tienen acceso a este boton
+          this.router.navigate(['/pedido']);
+        } else {
+          this.error = true;
+          this.openSnackBar(res['mensaje'], 'ok');
         }
-        this.router.navigate([this.auth.redirectUrl]);
-      } else {
-        this.error = true;
-        this.openSnackBar(res['mensaje'], 'ok');
-      }
-    });
-      // this.auth.loginEmpleado(this.login)
-      //   .then(
-      //     response => {
-      //       console.log(response);
-      //       if (response['Estado'] === 'OK') {
-      //         localStorage.setItem('token', response['Token']);
-      //         if (!this._auth.redirectUrl) {
-      //           this._auth.redirectUrl = 'empleados';
-      //         }
-      //         this._router.navigate([this._auth.redirectUrl]);
-      //       } else {
-      //         this.error = true;
-      //         this.errorMessage = response['Mensaje'];
-      //       }
-      //     }
-      //   )
-      //   .catch(
-      //     response => {
-      //       this.error = true;
-      //       this.errorMessage = response['Mensaje'];
-      //     }
-      //   );
+      });
     } else {
       this.errorMessage = 'Debe completar los campos correctamente.';
       this.error = true;
@@ -96,23 +72,18 @@ export class LoginComponent implements OnInit {
   ingreso(tipo: string) {
     switch (tipo) {
       case 'socio':
-        console.log('login socio');
         this.form.setValue({ nombre: 'ezequiel', password: '123'});
         break;
       case 'mozo':
-        console.log('login mozo');
         this.form.setValue({ nombre: 'micaela', password: '123'});
         break;
       case 'cocinero':
-        console.log('login cocinero');
         this.form.setValue({ nombre: 'mariana', password: '123'});
         break;
       case 'bartender':
-        console.log('login bartender');
         this.form.setValue({ nombre: 'jose', password: '123'});
         break;
       case 'cervecero':
-        console.log('login cervecero');
         this.form.setValue({ nombre: 'pedro', password: '123'});
         break;
       default:
