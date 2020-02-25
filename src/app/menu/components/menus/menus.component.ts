08fe551bc2248975c52f6916851853e238ad637a
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-02-25 00:07:40 -0300

    se optimiza codigo de menu componets

--- src/app/menu/components/menus/menus.component.ts
+++ src/app/menu/components/menus/menus.component.ts
@@ -5,6 +5,7 @@ import { PedidoService } from '../../../core/services/pedido/pedido.service';
 import { Menu } from 'src/app/core/models/menu';
 import { Empleado } from '../../../core/models/empleado';
 import { Pedido } from '../../../core/models/pedido';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menus',
@@ -23,39 +24,40 @@ export class MenusComponent implements OnInit {
 
   constructor(
     private menuService: MenuService,
-    private pedidoService: PedidoService
-  ) { }
+    private pedidoService: PedidoService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.pedido = JSON.parse(localStorage.getItem('pedido'));
+    if (!this.pedido) {
+      this.router.navigate(['/mesa']);
+    }
     this.fetchMenu();
   }
 
   clickMenu(id: number) {
     this.menu = this.menus.find(menu => menu.id_menu === id ? menu : null);
     this.empleado = JSON.parse(localStorage.getItem('empleado'));
-    this.pedido = JSON.parse(localStorage.getItem('pedido'));
 
     // Seteo el horario de inicio del pedido
-    const horaInicio = new Date(); //Y-m-d H:i:s'
+    const horaInicio = new Date(); // Y-m-d H:i:s'
     const horaEntrega = new Date();
     horaEntrega.setTime(horaEntrega.getTime());
-    this.pedidoFecha = horaInicio.getUTCFullYear().toString() + '/' +
-                       horaInicio.getMonth().toString() + '/' +
-                       horaInicio.getDate().toString() + ' ' +
-                       horaInicio.getHours().toString() + ':' +
-                       horaInicio.getMinutes().toString() + ':' +
-                       horaInicio.getSeconds().toString();
+    // this.pedidoFecha = horaInicio.getUTCFullYear().toString() + '-' +
+    //                    horaInicio.getMonth().toString() + '-' +
+    //                    horaInicio.getDate().toString();
+    this.pedidoFecha = horaInicio.toISOString().slice(0, 10);
 
     this.pedido.fecha_pedido = this.pedidoFecha;
-    this.horaInicio = this.pedidoFecha;
+    this.horaInicio = horaInicio.getHours().toString() + ':' +
+                      horaInicio.getMinutes().toString() + ':' +
+                      horaInicio.getSeconds().toString();
 
     // Setea 30 min mas a la hora de inicio
     horaEntrega.setMinutes(horaEntrega.getMinutes() + 30);
 
-    this.horaEntrega = horaEntrega.getUTCFullYear().toString() + '/' +
-                       horaEntrega.getMonth().toString() + '/' +
-                       horaEntrega.getDate().toString() + ' ' +
-                       horaEntrega.getHours().toString() + ':' +
+    this.horaEntrega = horaEntrega.getHours().toString() + ':' +
                        horaEntrega.getMinutes().toString() + ':' +
                        horaEntrega.getSeconds().toString();
 
@@ -66,6 +68,7 @@ export class MenusComponent implements OnInit {
     this.pedido.hora_inicio_pedido = this.horaInicio;
     this.pedido.hora_estimada_entrega_pedido = this.horaEntrega;
     this.pedido.id_menu = this.menu.id_menu;
+    this.pedido.id_tipo_menu = this.menu.id_tipo;
 
     console.log(this.pedido);
     this.createPedido(this.pedido);
