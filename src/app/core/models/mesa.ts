b4c8b1e7022f3b524e2622558bb2cfb37562d8a2
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2020-04-04 16:31:49 -0300

    se modifica modelo de mesa

--- src/app/core/models/mesa.ts
+++ src/app/core/models/mesa.ts
@@ -1,7 +1,7 @@
 export interface Mesa {
-    id_mesa: number;
-    codigo_mesa: string;
-    id_estado_mesa: number;
-    descripcion_estado_mesa: string;
-    foto_mesa: string;
+    id_mesa?: number;
+    codigo_mesa?: string;
+    id_estado_mesa?: number;
+    descripcion_estado_mesa?: string;
+    foto_mesa?: string;
 }
