543eb5f6d2702af5b481be63043b3dfd04b749df
Ezequiel A. Freire <ezequiel.a.freire@gmail.com>
2020-07-25 16:41:05 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,13 +7,6 @@ export const environment = {
   // url_api: 'http://localhost:8080',
   url_api: 'https://comandautn.herokuapp.com',
   firebase: {
-    apiKey: 'AIzaSyC7-u68TuEYo5nNAq1V5GxLLhcCw5Ga8yg',
-    authDomain: 'comandautn.firebaseapp.com',
-    databaseURL: 'https://comandautn.firebaseio.com',
-    projectId: 'comandautn',
-    storageBucket: 'comandautn.appspot.com',
-    messagingSenderId: '1080922447740',
-    appId: '1:1080922447740:web:ec8a7082032f537ab30ef9'
   }
 
 };
